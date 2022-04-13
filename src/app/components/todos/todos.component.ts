e129279346af003ca9be6d1d30ff9cef15b32d6c
Vazgen Ayrapetyan <vazgen.ayrapetyan@united.com>
2022-04-13 19:31:38 +0200

    Progress

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -9,6 +9,7 @@ import { Todo } from 'src/app/models/Todo';
 export class TodosComponent implements OnInit {
 
   todos: Todo[];
+  inputTodo: string = '';
 
   constructor() { }
 
@@ -39,5 +40,13 @@ export class TodosComponent implements OnInit {
 
   deleteTodo (id: number) {
    this.todos = this.todos.filter((v, i) => i !== id); 
+  }
+
+  addTodo() {
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false
+    })
+    this.inputTodo = '';
   }
 }
